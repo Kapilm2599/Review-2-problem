f023b59f89201e8b9546b709d9b16473cb6d06ce
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-18 21:54:16 +0530

    Added: Create an Employee at a multiple company

--- src/com/blz/empwage/CompanyMain.java
+++ src/com/blz/empwage/CompanyMain.java
@@ -1,7 +1,11 @@
 package com.blz.empwage;
 
+import java.util.*;
 public class CompanyMain {
+	public void main (String[] args) {
+		Map<String, Company> companyMap = new HashMap<>();
+		Company company1 = new Company("ABC Company");
+	}
 	
-	Company newComapny = new Company();
 	
 	}
--- src/com/blz/empwage/Employee.java
+++ src/com/blz/empwage/Employee.java
@@ -6,52 +6,39 @@ import java.util.Scanner;
 //-> Also Maintain a dictionary of Company through a Map(key->companyName , value->Company-Object) 
 //->  Create 3 classes - Company , Employee and CompanyMain
 public class Employee {
-	
-	
-	public static void main(String[] args) {
-		
+			
 		//Create an employee details
-		private String name, city, state, department;
+		private String Name, city, state, department;
 		private int age;
 		private long phoneNumber;
+		
+		Public Employee(String name, int age, long phoneNumber, String city, String state, String department) {
+			this.name = name;
+			this.age = age;
+			this.phoneNumber = phoneNumber;
+			this.city = city;
+			this.state = state;
+			this.department = department;
+			
+		}
+			
+		}
 		
 		public String getName() {
 			return name;
 		}
-		public void  setName(String name){
-			this.name = name;
-		} 
-		
 		public int getAge() {
 			return age;
 		}
-		public void setAge (String age) {
-			this.age = age;
-		}
-		
+
 		public long getphoneNumber() {
 			return phoneNumber;
 		}
-		public void getPhoneNumber(long phoneNumber) {
-			this.PhoneNumber = phoneNumber;
-		}
-		
 		public String getCity() {
 			return city;
 		}
-		public void  setCity(String city){
-			this.city = city;
-		} 
-		
 		public String getState() {
 			return state;
-		}
-		public void  setState(String state){
-			this.state = state;
-		} 
-		
-		public String getDepartment() {
-			return department;
 		}
 		public void  setDepartment(String department){
 			this.department = department;
