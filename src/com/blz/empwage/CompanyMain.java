44d07efd82fcd95b39a4f6eac1d0b89393c39cf9
Kapilm2599 <111445779+Kapilm2599@users.noreply.github.com>
2023-04-24 18:37:28 +0530

    Added: Create an Employee at a multiple company

--- src/com/blz/empwage/CompanyMain.java
+++ src/com/blz/empwage/CompanyMain.java
@@ -10,8 +10,22 @@ public class CompanyMain {
         company1.addEmployee(new Employee("Sagar Choudhary", 25, "896352417", "Pune", "MH", "Marketing"));
         company1.addEmployee(new Employee("Rahul Deshmukh", 35, "7985623210", "Mumbai", "MH", "Engineering"));
         companyMap.put(company1.getCompanyName(), company1);
+	
+	Company company2 = new Company("XYZ Company");
+        company2.addEmployee(new Employee("Alice Lee", 27, "555-3456", "Los Angeles", "CA", "Sales"));
+        company2.addEmployee(new Employee("Mike Chen", 32, "555-7890", "San Francisco", "CA", "Engineering"));
+        company2.addEmployee(new Employee("Karen Davis", 29, "555-2345", "Seattle", "WA", "Marketing"));
+        companyMap.put(company2.getCompanyName(), company2);
 
-  
+        // print the employees of each company
+        for (String companyName : companyMap.keySet()) {
+            Company company = companyMap.get(companyName);
+            System.out.println("Employees of " + company.getCompanyName() + ":");
+            for (Employee employee : company.getEmployee()) {
+                System.out.println(employee.getName() + " (" + employee.getDepartment() + ")");
+            }
+            System.out.println();      
+         }
         
     }
 }
